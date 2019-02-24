f649ee028e4dd7e6a047d54363af7517e1023e12
Tuan Le <tuan.t.lei@gmail.com>
2019-02-23 17:54:22 -0800

    Demo code updated

--- demo/Demo.js
+++ demo/Demo.js
@@ -9,6 +9,8 @@ import {
     StatusBar
 } from 'react-native';
 
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
 import SearchHeader from 'react-native-search-header';
 
 const DEVICE_WIDTH = Dimensions.get(`window`).width;
@@ -24,24 +26,24 @@ const styles = StyleSheet.create({
         zIndex: 10,
         elevation: 2,
         width: DEVICE_WIDTH,
-        height: 21,
+        height: 36,
         backgroundColor: '#0097a7'
     },
     header: {
         flexDirection: `row`,
-        justifyContent: 'space-between',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
         width: DEVICE_WIDTH,
         height: 56,
-        marginBottom: 6,
+        padding: 6,
         backgroundColor: '#00bcd4'
     },
     label: {
         flexGrow: 1,
         fontSize: 18,
         fontWeight: `600`,
-        textAlign: `left`,
-        paddingLeft: 150,
+        textAlign: `right`,
+        paddingRight: 50,
         marginVertical: 8,
         paddingVertical: 3,
         color: `#f5fcff`,
@@ -58,26 +60,36 @@ const styles = StyleSheet.create({
     }
 });
 
-export default class Demo extends Component {
+class Example1 extends Component {
     constructor (props) {
         super(props);
+        this.searchHeader = null;
     }
     render () {
+        const component = this;
+        const {
+            navigation
+        } = component.props;
         return (
             <View style = { styles.container }>
                 <StatusBar barStyle = 'light-content' />
                 <View style = { styles.status }/>
                 <View style = { styles.header }>
-                    <Text style = { styles.label } >Demo </Text>
+                    <Text style = { styles.label } > Example 1 </Text>
                     <Button
                         title = 'Search'
                         color = '#f5fcff'
-                        onPress = {() => this.searchHeader.show()}
+                        onPress = {() => component.searchHeader.show()}
+                    />
+                    <Button
+                        title = 'Next'
+                        color = '#f5fcff'
+                        onPress = {() => navigation.navigate(`example2`)}
                     />
                 </View>
                 <SearchHeader
                     ref = {(searchHeader) => {
-                        this.searchHeader = searchHeader;
+                        component.searchHeader = searchHeader;
                     }}
                     placeholder = 'Search...'
                     placeholderColor = 'gray'
@@ -86,12 +98,12 @@ export default class Demo extends Component {
                     persistent = { false }
                     enableSuggestion = { true }
                     entryAnimation = 'from-right-side'
-                    topOffset = { 21 }
+                    topOffset = { 36 }
                     iconColor = 'gray'
                     iconImageComponents = {[{
                         name: `hide`,
                         customStyle: {
-                            tintColor: 'blue'
+                            tintColor: 'red'
                         }
                     }]}
                     onClear = {() => {
@@ -114,26 +126,236 @@ export default class Demo extends Component {
                     onSearch = {(event) => {
                         console.log(event.nativeEvent.text);
                     }}
+                    style = {{
+                        header: {
+                            height: 56,
+                            backgroundColor: `#fdfdfd`
+                        }
+                    }}
                 />
                 <View style = { styles.button }>
                     <Button
-                        title = 'Clear Suggestion'
+                        title = 'Show'
                         color = '#f5fcff'
-                        onPress = {() => {
-                            this.searchHeader.clearSuggestion();
-                        }}
+                        onPress = {() => component.searchHeader.show()}
+                    />
+                </View>
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Hide'
+                        color = '#f5fcff'
+                        onPress = {() => component.searchHeader.hide()}
                     />
                 </View>
                 <View style = { styles.button }>
                     <Button
                         title = 'Clear'
                         color = '#f5fcff'
-                        onPress = {() => {
-                            this.searchHeader.clear();
-                        }}
+                        onPress = {() => component.searchHeader.clear()}
+                    />
+                </View>
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Clear'
+                        color = '#f5fcff'
+                        onPress = {() => component.searchHeader.clear()}
+                    />
+                </View>
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Next'
+                        color = '#f5fcff'
+                        onPress = {() => navigation.navigate(`example2`)}
                     />
                 </View>
             </View>
+        );
+    }
+}
+
+class Example2 extends Component {
+    constructor (props) {
+        super(props);
+        this.searchHeader = null;
+    }
+    render () {
+        const component = this;
+        const {
+            navigation
+        } = component.props;
+        return (
+            <View style = { styles.container }>
+                <StatusBar barStyle = 'light-content' />
+                <View style = { styles.status }/>
+                <View style = { styles.header }>
+                    <Button
+                        title = 'Prev'
+                        color = '#f5fcff'
+                        onPress = {() => navigation.goBack()}
+                    />
+                    <Text style = { styles.label } > Example 2 </Text>
+                    <Button
+                        title = 'Search'
+                        color = '#f5fcff'
+                        onPress = {() => component.searchHeader.show()}
+                    />
+                </View>
+                <SearchHeader
+                    ref = {(searchHeader) => {
+                        component.searchHeader = searchHeader;
+                    }}
+                    placeholder = 'Search...'
+                    placeholderColor = 'gray'
+                    dropShadowed = { false }
+                    autoFocus = { true }
+                    visibleInitially = { true }
+                    persistent = { false }
+                    enableSuggestion = { true }
+                    entryAnimation = 'from-left-side'
+                    topOffset = { 36 }
+                    iconColor = 'gray'
+                    iconImageComponents = {[{
+                        name: `hide`,
+                        customStyle: {
+                            tintColor: 'red'
+                        }
+                    }]}
+                    onClear = {() => {
+                        console.log(`CLEAR`);
+                    }}
+                    onGetAutocompletions = {async (text) => {
+                        if (text) {
+                            const response = await fetch(`http://suggestqueries.google.com/complete/search?client=firefox&q=${text}`, {
+                                method: `get`
+                            });
+                            const data = await response.json();
+                            return data[1];
+                        } else { // eslint-disable-line
+                            return [];
+                        }
+                    }}
+                    onEnteringSearch = {(event) => {
+                        console.log(event.nativeEvent.text);
+                    }}
+                    onSearch = {(event) => {
+                        console.log(event.nativeEvent.text);
+                    }}
+                    style = {{
+                        header: {
+                            height: 48,
+                            backgroundColor: `#fdfdfd`
+                        },
+                        input: {
+                            borderRadius: 4,
+                            backgroundColor: `#b2c9d4`
+                        }
+                        // suggestion: {
+                        //     backgroundColor: `red`
+                        // }
+                    }}
+                />
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Show'
+                        color = '#f5fcff'
+                        onPress = {() => component.searchHeader.show()}
+                    />
+                </View>
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Hide'
+                        color = '#f5fcff'
+                        onPress = {() => component.searchHeader.hide()}
+                    />
+                </View>
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Clear'
+                        color = '#f5fcff'
+                        onPress = {() => component.searchHeader.clear()}
+                    />
+                </View>
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Clear'
+                        color = '#f5fcff'
+                        onPress = {() => component.searchHeader.clear()}
+                    />
+                </View>
+                <View style = { styles.button }>
+                    <Button
+                        title = 'Prev'
+                        color = '#f5fcff'
+                        onPress = {() => navigation.goBack()}
+                    />
+                </View>
+            </View>
+        );
+    }
+}
+
+
+const ExampleStackNavigator = createStackNavigator({
+    example1: {
+        screen: ({
+            navigation
+        }) => {
+            return (
+                <Example1 navigation = { navigation }/>
+            );
+        },
+        navigationOptions: () => {
+            return {
+                header: {
+                    visible: true
+                }
+            };
+        }
+    },
+    example2: {
+        screen: ({
+            navigation
+        }) => {
+            return (
+                <Example2 navigation = { navigation }/>
+            );
+        },
+        navigationOptions: () => {
+            return {
+                header: {
+                    visible: true
+                }
+            };
+        }
+    }
+}, {
+    initialRouteName: `example1`,
+    mode: `card`,
+    headerMode: `none`,
+    cardStyle: {
+        backgroundColor: `transparent`
+    }
+});
+
+const AppContainer = createAppContainer(ExampleStackNavigator);
+
+export default class Demo extends Component {
+    constructor (props) {
+        super(props);
+
+        this.refCache = {
+            searchHeader1: null,
+            searchHeader2: null
+        };
+    }
+    render () {
+        const component = this;
+        return (
+            <AppContainer
+                screenProps = {{
+                    component
+                }}
+            />
         );
     }
 }
